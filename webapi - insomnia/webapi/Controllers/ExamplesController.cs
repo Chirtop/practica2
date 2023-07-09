c22cd890dca1f5c8867325da68901360eac07162
Chirtop Catalin <catalinioan.chirtop@ulbsibiu.ro>
2023-07-09 19:45:43 +0300

    new files added

---
+++ webapi - insomnia/webapi/Controllers/ExamplesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ExamplesController : Controller
+{
+    [HttpGet("{id}")]
+    public ActionResult<string> Get(int id, [FromHeader] string token, [FromQuery] string term)
+    {
+        string message = id.ToString() + " " + token + " " + term;
+        if (token != "abc")
+        {
+            return Unauthorized("unauthorized");
+        }
+        return Ok(message);
+    }
+    
+    [HttpPost]
+    public ActionResult<string> Get(int id, [FromHeader] string token, [FromQuery] string term, [FromBody] POSTResponseBody postBody)
+    {
+        // string message = id.ToString() + " " + token + " " + term;
+        // if (token != "abc")
+        // {
+        //     return Unauthorized("unauthorized");
+        // }
+        
+        return Ok(postBody.SomeString + " " + postBody.SomeInt);
+    }
+}
+
+public class POSTResponseBody
+{
+    public bool SomeBool { get; set; }
+    public string SomeString { get; set; }
+    public int SomeInt { get; set; }
+}
---
+++ webapi - insomnia/webapi/Controllers/PersonsController.cs
@@ -0,0 +1,80 @@
+﻿// using Microsoft.AspNetCore.Mvc;
+// using System;
+// using System.IO;
+// using Newtonsoft.Json;
+//
+// namespace webapi.Controllers;
+//
+// [Route("[controller]")]
+// [ApiController]
+// public class PersonsController : Controller
+// {
+//     [HttpGet]
+//     public ActionResult<string> Get(int id, [FromHeader] string token, [FromQuery] string term)
+//     {
+//         string jsonContents = System.IO.File.ReadAllText("Persons.json");
+//         Person[] persons =  JsonConvert.DeserializeObject<Person[]>(jsonContents);
+//         
+//         for (int i = 0; i < persons.Length; i++)
+//         {
+//             if(persons[i].email.Contains(term)) 
+//                 return Ok(persons[i].email);
+//                 
+//         }
+//         return NotFound();
+//     }
+// }
+//
+// public class Person
+// {
+//     public int id { get; set; }
+//     public string email { get; set; }
+//     public string company { get; set; }
+//     public string name { get; set; }
+//     public string age { get; set; }
+//     
+// }
+
+
+
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace webapi.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class PersonsController : Controller
+{
+    [HttpGet]
+    public ActionResult<string> Get(int id, [FromHeader] string token, [FromQuery] string term)
+    {
+        string jsonContents = System.IO.File.ReadAllText("Persons.json");
+        Person[] persons =  JsonConvert.DeserializeObject<Person[]>(jsonContents);
+        List<string> searchResult = new List<string>();
+        
+            for (int i = 0; i < persons.Length; i++)
+            {
+                if(persons[i].email.Contains(term))
+                    searchResult.Add(persons[i].email);
+            }
+            if (searchResult.Count != 0)
+            {
+                return Ok(searchResult);
+            }
+            
+        return NotFound();
+    }
+}
+
+public class Person
+{
+    public int id { get; set; }
+    public string email { get; set; }
+    public string company { get; set; }
+    public string name { get; set; }
+    public string age { get; set; }
+    
+}
